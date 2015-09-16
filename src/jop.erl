622542e499d7637e9ed42c452662fb0d77fad2dd
Renaud Mariana <rmariana@ocs.online.net>
2015-09-16 10:19:37 +0200

    filename as iolist

--- src/jop.erl
+++ src/jop.erl
@@ -1,7 +1,19 @@
+%% Copyright (c) 2015, Renaud Mariana <rmariana@gmail.com>
+%%
+%% Permission to use, copy, modify, and/or distribute this software for any
+%% purpose with or without fee is hereby granted, provided that the above
+%% copyright notice and this permission notice appear in all copies.
+%%
+%% THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+%% WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+%% MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+%% ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+%% WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+%% ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+%% OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+
 -module(jop).
 
-% in memory journal of operations library
-%% (fast) log in ets {key, operation} 
 -export([init/1, log/3, dump/1, clear/1]).
 -define(tag_start,<<"jop_start">>).
 -compile({inline,log/3}).
@@ -20,12 +32,12 @@ log(Tab, Key, Op) -> ets:insert(Tab, {Key, Op, erlang:monotonic_time(micro_secon
 
 %% dump ets logs in ordered timestamps & ordered keys files
 %% delete also the ets log
--spec dump(Tab::atom()) -> string().
+-spec dump(Tab::atom()) -> iolist().
 dump(Tab) -> 
     Date = date_str(),
     Fname = fname(Tab, Date),
-    {ok, Fd} = file:open(Fname++"dates", [write]),
-    {ok, Fb} = file:open(Fname++"keys", [write]),
+    {ok, Fd} = file:open([Fname,"dates"], [write]),
+    {ok, Fb} = file:open([Fname,"keys"], [write]),
     [{_,_,T0}] = ets:lookup(Tab, ?tag_start),
     ets:delete(Tab, ?tag_start),
     [io:format(Fd, "~s ~w: ~w~n", [time_us(T-T0), K, B]) 
@@ -37,7 +49,7 @@ dump(Tab) ->
     Fname.
 
 %%@private
-fname(Tab, Date) -> "jop_"++atom_to_list(Tab)++Date++"_".
+fname(Tab, Date) -> ["jop_", atom_to_list(Tab), Date, "_"].
 	
 date_str() ->
     HMS = lists:flatten([tuple_to_list(Y)|| Y <- tuple_to_list(calendar:universal_time_to_local_time(calendar:universal_time()))]),
